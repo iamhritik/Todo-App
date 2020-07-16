3289e9b1213f75fc4ebe620a3482d61768916176
iamhritik <hritik.99@outlook.com>
2020-07-16 17:20:15 +0530

    add something from the first branch.

--- main/views.py
+++ main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.utils import timezone
 from main.models import todo
-from django.http import HttpResponseRedirect
+from django.http import HttpResponseRedirect, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
@@ -11,11 +11,14 @@ def home(request):
 
 @csrf_exempt
 def add_task(request):
-  content = request.POST["content"]
-  content_add_time = timezone.now()
-  todo.objects.create(text=content,added_date=content_add_time)
-  print(todo.objects.all().count())
-  return HttpResponseRedirect('/')
+  if request.method == "POST":
+    task_h = request.POST["task_h"]
+    task_d = request.POST["task_d"]
+    content_add_time = timezone.now()
+    if todo.objects.create(task_h=task_h,task_d=task_d,added_date=content_add_time):
+      return HttpResponseRedirect('add_task'),HttpResponse("Task added")
+  else:
+    return render(request, "add.html")
 
 @csrf_exempt
 def del_task(request, num):
